7fabc7be40216b97b40e2f965817629bd3ca8b39
Jordan Minatogawa <jordankm@jordankm-macbookpro2.roam.corp.google.com>
2019-01-13 16:44:57 -0800

    maybe finished with knn. need to verify

--- hw1_knn.py
+++ hw1_knn.py
@@ -11,6 +11,7 @@ from collections import Counter
 # DO NOT MODIFY ABOVE CODES
 ############################################################################
 import heapq
+import hw1_utils
 
 class KNN:
 
@@ -20,18 +21,18 @@ class KNN:
         self.training_examples = {}
 
     def train(self, features: List[List[float]], labels: List[int]):
-        for index in len(features):
-            training_examples[features[index]] = labels[index] 
+        for index in range(0,len(features)):
+            self.training_examples[tuple(features[index])] = labels[index] 
         
     
     def predict(self, features: List[List[float]]) -> List[int]:
         predicted_classes = []
-        for index in len(features):
-            k_nearest = get_k_neighbors(features[index])
-            predicted_classes.append(_get_class_from_neighbors(k_nearest))
+        for index in range(0,len(features)):
+            k_nearest = self.get_k_neighbors(features[index])
+            predicted_classes.append(self._get_class_from_neighbors(k_nearest))
         return predicted_classes
 
-    def _get_class_from_neighbors(k_nearest):
+    def _get_class_from_neighbors(self, k_nearest):
         counter = Counter(k_nearest)
         label, count = counter.most_common()[0]
         return label
@@ -43,10 +44,11 @@ class KNN:
         distances_to_features = {} 
         distances = set()
         for features in self.training_examples.keys():
+            features = list(features)
             distance = self.distance_function(point, features)
             feature_list = []
             if distance in distances_to_features:
-                feature_list = distance_to_features[distance]
+                feature_list = distances_to_features[distance]
             feature_list.append(features)
 
             distances.add(distance)
@@ -57,9 +59,9 @@ class KNN:
         for distance in smallest_distances:
             feature_list = distances_to_features[distance]
             for feature in feature_list:
-                if len(k_nearest == self.k):
+                if len(k_nearest) == self.k:
                     return k_nearest
-                k_nearest.append(self.training_examples[feature])
+                k_nearest.append(self.training_examples[tuple(feature)])
         return k_nearest
 
     # Gets the f1 score from given knn_classifier and f1_score function.
@@ -81,7 +83,9 @@ class KNN:
 
         while current_k < len(ytrain):
             current_k += 1
-            for distance_function in distance_funcs:
+            for distance_function_key in distance_funcs.keys():
+                distance_function = distance_funcs[distance_function_key]
+
                 knn_classifier = KNN(current_k, distance_function)
                 knn_classifier.train(Xtrain, ytrain)
                 knn_score = _get_f1_score_from_classifier(knn_classifier, Xval, yVal)
@@ -104,25 +108,59 @@ class KNN:
 
         return best_k, model
     
-    #TODO: Complete the model selection function where you need to find the best k with transformation
+    # Complete the model selection function where you need to find the best k with transformation
     def model_selection_with_transformation(distance_funcs,scaling_classes, Xtrain, ytrain, f1_score, Xval, yval, Xtest, ytest):
+        model = None
+        best_f1_score = None
+        best_k = None
+
+        for scaling_class_key in scaling_classes.keys():
+            scaling_class = scaling_classes[scaling_class_key]
+            scaler = scaling_class()
+            scaled_Xtrain = scaler(Xtrain)
+            scaled_ytrain = scaler(ytrain)
+            scaled_Xval = scaler(Xval)
+            scaled_yval = scaler(yval)
+            scaled_Xtest = scaler(Xtest)
+            scaled_ytest = scaler(ytest)
+
+            best_k_with_scaled , best_model_with_scaled = KNN.model_selection_without_transformation(
+                    distance_funcs, scaled_Xtrain, scaled_ytrain, f1_score, scaled_Xval, scaled_yval, scaled_Xtest, scaled_ytest)
+            best_f1_score_with_scaled = _get_f1_score_from_classifier(best_model_with_scaled, scaled_Xval, scaled_yval, f1_score)
+            if best_f1_score == None or best_f1_score_with_scaled > best_f1_score:
+                best_f1_score = best_f1_score_with_scaled
+                model = best_model_with_scaled
+                best_k = best_k_with_scaled
+
+
         
-                #Dont change any print statement
-                print('[part 1.2] {name}\t{scaling_name}\tk: {k:d}\t'.format(name=name, scaling_name=scaling_name, k=k) +
-                          'train: {train_f1_score:.5f}\t'.format(train_f1_score=train_f1_score) + 
-                          'valid: {valid_f1_score:.5f}'.format(valid_f1_score=valid_f1_score))
-    
-                print()
-                print('[part 1.2] {name}\t{scaling_name}\t'.format(name=name, scaling_name=scaling_name) +
-                      'best_k: {best_k:d}\ttest: {test_f1_score:.5f}'.format(best_k=best_k, test_f1_score=test_f1_score))
-                print()
+        #Dont change any print statement
+        print('[part 1.2] {name}\t{scaling_name}\tk: {k:d}\t'.format(name=name, scaling_name=scaling_name, k=k) +
+                  'train: {train_f1_score:.5f}\t'.format(train_f1_score=train_f1_score) + 
+                  'valid: {valid_f1_score:.5f}'.format(valid_f1_score=valid_f1_score))
+
+        print()
+        print('[part 1.2] {name}\t{scaling_name}\t'.format(name=name, scaling_name=scaling_name) +
+              'best_k: {best_k:d}\ttest: {test_f1_score:.5f}'.format(best_k=best_k, test_f1_score=test_f1_score))
+        print()
+        return best_k, model
         
         
-    #TODO: Do the classification 
+    # Do the classification 
     def test_classify(model):
+        training_features = [[2,1], [3,2], [4,3] , [1,2], [2,3]]
+        training_labels = [1,1,1,0,0]
+        model.train(training_features, training_labels)
+        print(str(model.predict([[1,3]])))
+        
+
         
 
 if __name__ == '__main__':
-    print(numpy.__version__)
+    model = KNN(5, hw1_utils.euclidean_distance)
+    KNN.test_classify(model)
+
+
+    print(np.__version__)
     print(scipy.__version__)
 
--- hw1_utils.py
+++ hw1_utils.py
@@ -16,6 +16,9 @@ def f1_score(real_labels: List[int], predicted_labels: List[int]) -> float:
         real_total += real_labels[index]
         predicted_total += predicted_labels[index]
 
+    if real_total + predicted_total == 0:
+        return 0
+
     return (2 * numerator) / (real_total + predicted_total)
 
 def euclidean_distance(point1: List[float], point2: List[float]) -> float:
@@ -54,6 +57,89 @@ def cosine_sim_distance(point1: List[float], point2: List[float]) -> float:
     point2Norm = math.sqrt(point2Norm)
     return total / (point1Norm * point2Norm)
 
+def normalization_scaler(features: List[List[float]]) -> List[List[float]]:
+    scaled_features = []
+    for feature in features:
+        denominator = 0
+        for index in range(0, len(feature)):
+            denominator += feature[index] ** 2
+        denominator = math.sqrt(denominator)
+        new_feature = []
+        for index in range(0, len(feature)):
+            if denominator == 0:
+                new_feature.append(0)
+            else:
+                new_feature.append(feature[index] / denominator)
+        scaled_features.append(new_feature)
+    return scaled_features
+
+class MinMaxScaler:
+    """
+    You should keep some states inside the object.
+    You can assume that the parameter of the first __call__
+        must be the training set.
+    Note:
+        1. you may assume the parameters are valid when __call__
+            is being called the first time (you can find min and max).
+    Example:
+        train_features = [[0, 10], [2, 0]]
+        test_features = [[20, 1]]
+        scaler = MinMaxScale()
+        train_features_scaled = scaler(train_features)
+        # now train_features_scaled should be [[0, 1], [1, 0]]
+        test_features_sacled = scaler(test_features)
+        # now test_features_scaled should be [[10, 0.1]]
+        new_scaler = MinMaxScale() # creating a new scaler
+        _ = new_scaler([[1, 1], [0, 0]]) # new trainfeatures
+        test_features_scaled = new_scaler(test_features)
+        # now test_features_scaled should be [[20, 1]]
+    """
+
+    def __init__(self):
+        self.max_list = None 
+        self.min_list = None
+        
+
+    def __call__(self, features: List[List[float]]) -> List[List[float]]:
+        """
+        normalize the feature vector for each sample . For example,
+        if the input features = [[2, -1], [-1, 5], [0, 0]],
+        the output should be [[1, 0], [0, 1], [0.333333, 0.16667]]
+        """
+        if self.max_list == None and self.min_list == None:
+            self._create_max_min_list(features)
+
+        scaled_features = []
+        for feature in features:
+            scaled_feature = []
+            for index in range(0, len(feature)):
+                max_val = self.max_list[index]
+                min_val = self.min_list[index]
+                denominator = max_val - min_val
+                if denominator == 0:
+                    scaled_value = 0
+                else:
+                    scaled_value = (feature[index] - min_val) / (max_val - min_val)
+                scaled_feature.append(scaled_value)
+            scaled_features.append(scaled_feature)
+        return scaled_features
+                
+
+
+    def _create_max_min_list(self, features: List[List[float]]):
+        for feature in features:
+            for index in range(0, len(feature)):
+                if self.max_list == None and self.min_list == None:
+                    self.max_list = feature.copy() 
+                    self.min_list = feature.copy()
+                    break
+                else:
+                    self.max_list[index] = max(self.max_list[index], feature[index])
+                    self.min_list[index] = min(self.min_list[index], feature[index])
+
+
 
 if __name__ == "__main__":
-    print(gaussian_kernel_distance([1, 3], [3, 5]))
+    predicted_labels = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0]
+    real_labels = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1]
+    print(str(f1_score(real_labels, predicted_labels)))
